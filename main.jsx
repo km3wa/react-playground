8025b901b32f00c3a48dc685c2b62125ddec4915
km3wa <91877667+km3wa@users.noreply.github.com>
2021-10-28 21:29:41 +0200

    exo6 vraiment terminé (pause/restart clock)

--- main.jsx
+++ main.jsx
@@ -1,87 +1,53 @@
-/*class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date() };
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Bonjour, monde !</h1>
-                <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
-}*/
-
-const Clock = () =>{
+const Clock = () => {
     const [date, setDate] = React.useState(new Date());
-    const [exemple, setExemple] = React.useState({
-        test: "coucou",
-    });
     const [color, setColor] = React.useState("black");
+    const [isRunning, setIsRunning] = React.useState(true);
+
+    const intervalRef = React.useRef();
 
     // componentWillMount() { //beforeMount
     //   console.log("I'm gonna render");
     // }
 
-    React.useEffect(() => {
-        console.log("I'm gonna render");
-    }); // déclenché à CHAQUE render => componentWillMount
+    /*React.useEffect(() => {
+        //console.log("I'm gonna render");
+    }); // déclenché à CHAQUE render => componentWillMount*/
 
-    React.useEffect(() => {
-        console.log("first render");
-    }, []); // déclenché uniquement au PREMIER render => componentDidMount
+    React.useEffect(() => { setDate(new Date()) }, []) // componentDidMount
+
+    React.useEffect(() => { // dateDidUpdate
+        intervalRef.current = window.setInterval(() => {
+            setDate(new Date());
+            /*setExemple((prevState) => ({
+                ...prevState,
+                pouet: "bonjour",
+            }
+        ));*/
+        }, 1000)
+    }, [date]);
 
 
-    React.useEffect(() => {
-        console.log(date.toLocaleTimeString())
-
-    }, [date]) // déclenché UNIQUEMENT lorsque date sera modifiée => componentDidUpdate
-
-    React.useEffect(() => {
+    React.useEffect(() => { // colorDidUpdate
         console.log("new color is :", color);
-
     }, [color])
 
-    React.useEffect(() => {
+    React.useEffect(() => { //isRunningDidUpdate
+        console.log(isRunning);
+        isRunning ? setDate(new Date()) : window.clearInterval(intervalRef.current);
+        console.log(intervalRef);
+    }, [isRunning])
 
+    React.useEffect(() => { // componentWillMount
         return () => {
-            // Déclenché lorsque le composant va être "détruit" => componentWillUnmount
-            console.log('I`m gonna die');
-        }
+            window.clearInterval(intervalRef.current);
+        } // componentWillUnmount
     })
-
-    const changeDate = () => {
-        setDate(new Date());
-        setExemple((prevState) => ({
-            ...prevState,
-            pouet: "bonjour",
-        }));
-    };
 
     const changeColor = () => {
         let colorHex = "#"
-        for (let i = 1; i<=3; i++){
-            let colorValue = (Math.floor(Math.random() * 256)).toString(16)
-            if (colorValue.length < 2) colorValue = "0"+colorValue;
+        for (let i = 1; i <= 3; i++) {
+            let colorValue = (Math.floor(Math.random() * 256)).toString(16) // un nombre entre #0 et #ff
+            if (colorValue.length < 2) colorValue = "0" + colorValue;
             colorHex += colorValue;
         }
         setColor(colorHex);
@@ -91,16 +57,23 @@ const Clock = () =>{
         setColor("black")
     }
 
+    const changeClockStatus = () => {
+        setIsRunning(isRunning ? false : true);
+    }
+
     return (
+
         <div>
             <h1>Bonjour, monde !</h1>
-            <span style={{color: color}}>Il est {date.toLocaleTimeString()}.</span>
+            <h2 style={{ color: color }}>Il est {date.toLocaleTimeString()}.</h2>
+
             <button onClick={changeColor}>click to change color</button>
             <button onClick={resetColor}>click to reset color</button>
+
+            <button onClick={changeClockStatus}>{isRunning ? "pause clock" : "restart clock"}</button>
         </div>
     );
 }
-
 
 ReactDOM.render(
     <Clock />,
